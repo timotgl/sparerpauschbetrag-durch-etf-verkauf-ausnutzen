0ef90458f71d14976fdad3602062ed6f36460efb
Timo Taglieber <github@timotaglieber.de>
2022-12-21 10:17:18 +0100

    Translate header info and read it from package.json

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import SwitchLanguage from '../SwitchLanguage';
 import classNames from './Header.module.css';
+import project from '../../../package.json';
 
-const Header = () => (
-  <div className={classNames.Container}>
-    <SwitchLanguage />
-    <p>
-      <a href="https://github.com/timotgl/sparerpauschbetrag-durch-etf-verkauf-ausnutzen">
-        GitHub repo
-      </a>{' '}
-      | <a href="https://timotaglieber.de/">Author's homepage</a>
-    </p>
-  </div>
-);
+interface PackageJson {
+  author: {
+    url: string;
+  };
+  repository: {
+    url: string;
+  };
+}
+
+const Header = () => {
+  const [t] = useTranslation();
+  return (
+    <div className={classNames.Container}>
+      <SwitchLanguage />
+      <p>
+        <a href={(project as PackageJson).repository.url}>
+          {t('app.repo_url_label')}
+        </a>{' '}
+        | <a href={project.author.url}>{t('app.author_homepage_url_label')}</a>
+      </p>
+    </div>
+  );
+};
 
 export default Header;
