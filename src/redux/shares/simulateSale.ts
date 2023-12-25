07761919ade167087dad7e056792df2544530a16
Timo Taglieber <github@timotaglieber.de>
2023-12-25 17:49:01 +0100

    Translate text output of sale simulation algo

--- src/redux/shares/simulateSale.ts
+++ src/redux/shares/simulateSale.ts
@@ -1,3 +1,5 @@
+import i18next from 'i18next';
+
 import { Share } from './reducer';
 import { SHARE_DECIMAL_PLACES, CURRENCY_DECIMAL_PLACES } from '../../constants';
 
@@ -13,15 +15,14 @@ const simulateSale = (
   bidPrice: number,
   sharesRaw: Array<Share>
 ): SaleSimulationResult => {
-  let fractionedSharesWarning = 'None of the positions will be fractioned.';
+  const { t } = i18next;
+  let fractionedSharesWarning = t('sale_simulation.no_fractions');
 
   const shares = sharesRaw.map((share) => [
     share.amountPurchased - share.amountSold,
     share.price,
   ]);
   const numShares = shares.reduce((num, share) => num + share[0], 0);
-  console.log('numShares:', numShares);
-
   let outcomes: Array<string> = [];
 
   const simulateIncrementalSale = (numSharesSold: number) => {
@@ -36,20 +37,22 @@ const simulateSale = (
     let sharesSold = numSharesSold;
 
     while (sharesSold > 0 && sharesIndex < shares.length) {
-      //console.log('Shares left to sell:', sharesSold);
-
       const [originalAmount, originalPrice] = shares[sharesIndex];
       let amount = originalAmount;
 
       if (amount > sharesSold) {
         // The position is bigger than the number of shares we still need to sell
-        fractionedSharesWarning =
-          `Warning: The position with ${amount} ` +
-          `shares bought at ${originalPrice} ` +
-          `EUR will be reduced to ${Number(
-            (amount - sharesSold).toFixed(SHARE_DECIMAL_PLACES)
-          )} shares.\n` +
-          'Following FIFO, your next sale will start with this position.';
+        fractionedSharesWarning = t(
+          'sale_simulation.fractioned_shares_warning',
+          {
+            amount,
+            originalPrice: originalPrice.toFixed(CURRENCY_DECIMAL_PLACES),
+            purchaseDate: sharesRaw[sharesIndex].date,
+            reducedAmount: Number(
+              (amount - sharesSold).toFixed(SHARE_DECIMAL_PLACES)
+            ),
+          }
+        );
         amount = sharesSold;
       }
 
@@ -58,14 +61,11 @@ const simulateSale = (
           CURRENCY_DECIMAL_PLACES
         )
       );
-      const outcomeMessage =
-        profit > 0
-          ? `${amount.toFixed(
-              SHARE_DECIMAL_PLACES
-            )} shares sold with a profit of EUR ${profit}. ${(
-              originalAmount - amount
-            ).toFixed(SHARE_DECIMAL_PLACES)} shares are left.`
-          : '';
+      const outcomeMessage = t('sale_simulation.outcome_message', {
+        amount: amount.toFixed(SHARE_DECIMAL_PLACES),
+        profit: profit.toFixed(CURRENCY_DECIMAL_PLACES),
+        reducedAmount: (originalAmount - amount).toFixed(SHARE_DECIMAL_PLACES),
+      });
       outcomes.push(outcomeMessage);
 
       totalProfit += profit;
@@ -74,13 +74,6 @@ const simulateSale = (
     }
 
     totalProfit = Number(totalProfit.toFixed(CURRENCY_DECIMAL_PLACES));
-    /*
-    console.log(
-        'Total profit:', totalProfit,
-        'EUR for', numSharesSold,
-        'shares sold. Total sale value:', Number((numSharesSold * bidPrice).toFixed(2))
-    );
-    */
 
     return totalProfit;
   };
@@ -97,18 +90,6 @@ const simulateSale = (
       break;
     }
   }
-
-  console.log(
-    'You should sell',
-    numSharesToSell,
-    'shares to approximate but not exceed the optimal profit of',
-    optimalProfit,
-    'EUR with',
-    profit,
-    'EUR.'
-  );
-
-  console.log(fractionedSharesWarning);
 
   return {
     numSharesToSell,
